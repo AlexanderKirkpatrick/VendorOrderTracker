78b6e66a81636b4115bc5267a2d91ef361bd085a
Alexander <knibz@protonmail.com>
2022-07-16 13:54:47 -0400

    add routing and namespaces on cs and csproj files, add logic and routing to cshtml files

--- VendorOrderTracker/Startup.cs
+++ VendorOrderTracker/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ToDoList
+namespace VendorOrderTracker
 {
   public class Startup
   {
@@ -32,6 +32,8 @@ namespace ToDoList
       {
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
+
+      app.UseStaticFiles();
 
       app.Run(async (context) =>
       {
